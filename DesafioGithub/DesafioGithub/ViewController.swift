da4e086cc5137789ce187b9d029c1f683276adbc
Vinicius Soares Lima <vinicius.newescravo@gmail.com>
2023-05-11 15:03:32 -0300

    cria projeto e tela de erro

---
+++ DesafioGithub/DesafioGithub/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DesafioGithub
+//
+//  Created by Vinicius Soares Lima on 10/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let errorView = ErrorView()
+        errorView.setup(errorMessage: "teste de erro")
+        self.present(errorView, animated: true)
+    }
+}
+
---
+++ DesafioGithub/DesafioGithub/Views/ErrorView.swift
@@ -0,0 +1,45 @@
+//
+//  ErrorView.swift
+//  DesafioGithub
+//
+//  Created by Vinicius Soares Lima on 10/05/23.
+//
+
+import Foundation
+import UIKit
+
+class ErrorView: UIViewController {
+    private var label: UILabel = UILabel()
+    private let icon = UIImageView(image: UIImage(named: "erro"))
+    
+    override func viewDidLoad() {
+        setupView()
+        setupConstraints()
+    }
+    private func setupView() {
+        self.view.addSubview(label)
+        self.view.addSubview(icon)
+    }
+    private func setupConstraints() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            icon.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            icon.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            icon.widthAnchor.constraint(equalToConstant: 100),
+            icon.heightAnchor.constraint(equalTo: icon.widthAnchor),
+            label.topAnchor.constraint(equalTo: icon.bottomAnchor),
+            label.centerXAnchor.constraint(equalTo: icon.centerXAnchor),
+            label.widthAnchor.constraint(equalTo: self.view.widthAnchor, constant: -32)
+            ])
+    }
+    
+    func setup(errorMessage: String) {
+        label.text = errorMessage
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        
+    }
+}
+
+
