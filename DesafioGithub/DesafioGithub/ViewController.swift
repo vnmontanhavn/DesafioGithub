40f0972395c240cdaf241417d6a129516a191fed
Vinicius Soares Lima <vinicius.newescravo@gmail.com>
2023-05-12 15:38:11 -0300

    adiciona activity indicator durante o load da tela

--- DesafioGithub/DesafioGithub/ViewController.swift
+++ DesafioGithub/DesafioGithub/ViewController.swift
@@ -11,39 +11,65 @@ class ViewController: UIViewController {
 
     var tableView = UITableView()
     var users: [UserModel] = []
+    var indicator: UIActivityIndicatorView = UIActivityIndicatorView(style: .large)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .red
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-        self.tableView.register(UserListCell.self, forCellReuseIdentifier: UserListCell().getIdentifier())
-        self.view.addSubview(self.tableView)
+        title = "Usuarios"
+        setupTableView()
+        setupActivityIndicator()
         setupConstraints()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+        startIndicatorAnimation()
         UserListCaller().getList { response in
             print(response)
             self.users = response
             self.tableView.reloadData()
+            self.stopIndicatorAnimation()
         } fail: { errorString in
             let errorView = ErrorView()
             errorView.setup(errorMessage: errorString)
             self.present(errorView, animated: true)
+            self.stopIndicatorAnimation()
         }
+    }
+    
+    func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(UserListCell.self, forCellReuseIdentifier: UserListCell().getIdentifier())
+        self.view.addSubview(self.tableView)
+    }
+    
+    func setupActivityIndicator() {
+        self.view.addSubview(self.indicator)
+        indicator.hidesWhenStopped = true
+    }
+    
+    func startIndicatorAnimation() {
+        self.indicator.startAnimating()
+        self.tableView.isUserInteractionEnabled = false
+    }
+    
+    func stopIndicatorAnimation() {
+        self.indicator.stopAnimating()
+        self.tableView.isUserInteractionEnabled = true
     }
     
     func setupConstraints() {
         self.tableView.translatesAutoresizingMaskIntoConstraints = false
-        
+        self.indicator.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             self.tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
             self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
             self.tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
-            self.tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor)
+            self.tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            self.indicator.leftAnchor.constraint(equalTo: self.tableView.centerXAnchor),
+            self.indicator.centerYAnchor.constraint(equalTo: self.tableView.centerYAnchor)
             ])
     }
     
@@ -74,6 +100,11 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UserListCell().height()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let newView = UIViewController()
+        self.navigationController?.pushViewController(newView, animated: true)
     }
     
 }
