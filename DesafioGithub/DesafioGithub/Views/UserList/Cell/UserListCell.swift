6d6759706d6a48287167c88e6105a1316dfd4aed
Vinicius Soares Lima <vinicius.newescravo@gmail.com>
2023-05-12 15:03:36 -0300

    adiciona imagem nas células

--- DesafioGithub/DesafioGithub/Views/UserList/Cell/UserListCell.swift
+++ DesafioGithub/DesafioGithub/Views/UserList/Cell/UserListCell.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import SDWebImage
 
 class UserListCell: UITableViewCell {
     
@@ -29,6 +30,16 @@ class UserListCell: UITableViewCell {
         clearSetup()
         self.nameLabel.text = model.name
         self.gitLabel.text = model.gitURL
+        imageSetup(urlString: model.imageURL)
+    }
+    
+    func imageSetup(urlString: String) {
+        let url = URL(string: urlString)
+        self.image.translatesAutoresizingMaskIntoConstraints = false
+        self.image.contentMode = .scaleAspectFit
+        self.image.sd_setImage(with: url, placeholderImage: UIImage(named: "placeholder"))
+        self.image.clipsToBounds = true
+        self.image.layer.cornerRadius = 25
     }
     
     func setupConstraints() {
@@ -37,19 +48,20 @@ class UserListCell: UITableViewCell {
         image.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            image.topAnchor.constraint(equalTo: topAnchor),
-            image.leftAnchor.constraint(equalTo: leftAnchor),
-            image.bottomAnchor.constraint(equalTo: bottomAnchor),
-            nameLabel.leftAnchor.constraint(equalTo: image.rightAnchor),
-            nameLabel.topAnchor.constraint(equalTo: topAnchor),
+            image.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            image.leftAnchor.constraint(equalTo: leftAnchor, constant: 16),
+            image.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+            image.widthAnchor.constraint(equalTo: image.heightAnchor, multiplier: 1),
+            nameLabel.leftAnchor.constraint(equalTo: image.rightAnchor, constant: 8),
+            nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
             gitLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor),
-            gitLabel.leftAnchor.constraint(equalTo: image.rightAnchor)
+            gitLabel.leftAnchor.constraint(equalTo: image.rightAnchor, constant: 8)
             ])
     }
 }
 
 extension UserListCell: CellProtocol {
     func height() -> CGFloat {
-        return 50
+        return 70
     }
 }
