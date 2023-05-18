91c11fbda66abe36ad12d762ee76fcc7bda140ad
Vinicius Soares Lima <vinicius.newescravo@gmail.com>
2023-05-18 11:52:13 -0300

    adiciona mock nos teste de detalhes

--- DesafioGithub/DesafioGithubTests/DetailViewControllerTests.swift
+++ DesafioGithub/DesafioGithubTests/DetailViewControllerTests.swift
@@ -8,11 +8,31 @@
 import XCTest
 @testable import DesafioGithub
 
+class RepoMock: CallerProtocol {
+    var delegate: DesafioGithub.CallResponseDelegate?
+    required init(delegate: DesafioGithub.CallResponseDelegate?) {
+        self.delegate = delegate
+    }
+    
+    func call() {
+    }
+    func call(userURL: String?) {
+        let repository1 = RepositoryModel(name: "elemento 1", htmlUrl: "")
+        let repository2 = RepositoryModel(name: "elemento 2", htmlUrl: "")
+        let repository3 = RepositoryModel(name: "elemento 3", htmlUrl: "")
+        let list = [repository1, repository2, repository3]
+        delegate?.success(response: list)
+    }
+}
+
 final class DetailViewControllerTests: XCTestCase {
     var view: DetailViewController?
+    var mockDelegate: RepoMock?
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         view = DetailViewController()
+        mockDelegate = RepoMock(delegate: view)
+        view?.view.layoutIfNeeded()
         
     }
     
@@ -26,6 +46,8 @@ final class DetailViewControllerTests: XCTestCase {
         view?.setupController(model: userMock)
         XCTAssertEqual(view?.title, "testeNome")
         view?.viewDidLoad()
+        view?.repoCallDelegate = mockDelegate
+        view?.viewWillAppear(false)
         XCTAssertEqual(view?.view.backgroundColor, .blue)
         view?.tableView.reloadData()
         XCTAssertEqual(view?.tableView.visibleCells.count, 1)
